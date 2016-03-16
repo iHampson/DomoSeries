ea65259596fe413d0a9d2cad9d5e5c6194bb0c7d
Ian Hampson <inhampson@gmail.com>
2016-03-15 20:20:13 -0400

    Still working to get #17 work.

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ var router = app => {
   app.get('/login', controllers.Account.loginPage);
   app.get('/signup', controllers.Account.signupPage);
   app.get('/logout', controllers.Account.logout);
-  app.get('/maker', controllers.Account.makerPage);
+  app.get('/maker', controllers.Domo.makerPage);
   app.get('/', controllers.Account.loginPage);
 
   app.post('/login', controllers.Account.login);
