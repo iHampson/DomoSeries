358550d7fc05b5569f6fe6822325edb034e40a56
Ian Hampson <inhampson@gmail.com>
2016-04-01 17:33:21 -0400

    Altering a tag attribute 2.

--- client/maker.js
+++ client/maker.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
       console.log("Delete Clicked");
       var toDelete = e.target;
       var domoData = {
-        name: e.target.domoName;
+        name: e.target.domoName,
       };
       console.log(domoData);
       // sendAjax("/domoDelete",)
